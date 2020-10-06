54ea51a98598d293709f3c9549eb5dda87cc96d2
KodaiEzaki <59678647+KodaiEzaki@users.noreply.github.com>
2020-10-06 16:27:51 +0900

    Add files via upload

--- Thesis_Program_Translation.py
+++ Thesis_Program_Translation.py
@@ -3,3 +3,10 @@
 Program for master thesis from Matlab to Python
 """
 
+#前処理
+
+display('対象画像を選択')
+filename = uigetfile('*.png')
+RGB_image = imread(filename)
+[size_x, size_y, ~] = size(RGB_image)
+
