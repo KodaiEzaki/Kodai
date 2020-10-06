ee33757702b48f5ab966ce7952fb6e2a5557dd7b
KodaiEzaki <59678647+KodaiEzaki@users.noreply.github.com>
2020-10-06 17:00:06 +0900

    Add files via upload

--- Thesis_Program_Translation.py
+++ Thesis_Program_Translation.py
@@ -5,8 +5,16 @@ Program for master thesis from Matlab to Python
 
 #前処理
 
-display('対象画像を選択')
+import cv2
+
+print('対象画像を選択')
+
+RGB_image = cv2.imread('C:\Users\TANAKA\Desktop\プログラム\熱傷プログラム\プログラム\熱傷画像\正解ラベル\医師A\case01.png')
+
+"""
+display('')
 filename = uigetfile('*.png')
 RGB_image = imread(filename)
 [size_x, size_y, ~] = size(RGB_image)
+"""
 
